f491910aaa2242edc38f51e5751e1787ebeeb3d8
TeanaM <teana340@gmail.com>
2016-08-22 10:07:52 -0400

    Changing background
    Swiping.js instead of document.body... change to "document.getElementById..." then the name of each id

--- restaurants/swiping.js
+++ restaurants/swiping.js
@@ -54,17 +54,17 @@ $('body').on('click','.option li',function(){
 	$('#'+i+' .selected').attr('id',o);
 	$('#'+i+' .selected').text(v);
   if (v=="New York City"){
-  	document.body.style.backgroundImage = "url('http://www.ssn.tv/wp-content/uploads/2014/07/NYC-1.jpg')";
+  	document.getElementById('New York City').style.backgroundImage = "url('http://www.ssn.tv/wp-content/uploads/2014/07/NYC-1.jpg')";
   }
   else if (v=="Houston"){
-      document.body.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/d/de/Houston_night.jpg')";
+      document.getElementById('Houston').style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/d/de/Houston_night.jpg')";
     }
   else if (v=="Los Angeles"){
-      document.body.style.backgroundImage = "url('https://cdnx.jetcdn.com/static/images/destinations/los-angeles/bg3.jpg')";
+      document.getElementById('Los Angeles').style.backgroundImage = "url('https://cdnx.jetcdn.com/static/images/destinations/los-angeles/bg3.jpg')";
   }
   else if (v=="Boston"){
-      document.body.style.backgroundImage = "url('http://cache.boston.com/bonzai-fba/Original_Photo/2006/01/10/1136915281_2365.jpg')";
+      document.getElementById('Boston').style.backgroundImage = "url('http://cache.boston.com/bonzai-fba/Original_Photo/2006/01/10/1136915281_2365.jpg')";
   else {
-      document.body.style.backgroundImage = "url('http://www.visitphilly.com/thumb.php?w=1000&zc=1&src=http://photos.visitphilly.com/2014_CV_LOVE_PanoramaView-1400VP.jpg')";
+      document.getElementById('Philly').style.backgroundImage = "url('http://www.visitphilly.com/thumb.php?w=1000&zc=1&src=http://photos.visitphilly.com/2014_CV_LOVE_PanoramaView-1400VP.jpg')";
   }
 });
